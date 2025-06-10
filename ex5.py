e1ecfec2c5cb001cc9dbbcc8912c171f59b4166c
alsfelipe <felipealsantos17@gmail.com>
2025-06-10 09:57:17 -0300

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,34 @@
+print("Vamos lá, Vanellope! Vou te ajudar a montar um carro CINistro!")
+
+lista = []
+descartados = []
+roda = []
+corpo = []
+volante = []
+
+acabou = False
+while not acabou:
+    material = input()
+    separar =  material.split(" - ")
+
+    doce = separar[0]
+    qualidade = separar[1]
+    materiais_usados = separar.append([doce, qualidade])
+
+    if doce == "Mentos" or doce == "Jujuba":
+        if qualidade == "estragado":
+            
+        if qualidade == "alta qualidade":
+
+
+
+    if material == "Recursos Esgotados":
+        acabou = True
+    
+    elif material == "O REI DOCE ESTÁ ROUBANDO TODOS OS INGREDIENTES!":
+        doce = []
+        qualidade = []
+        print("Ah não!! Ele vai destruir seu carro, Vanellope! CUIDADO")
+        
+        
+    
